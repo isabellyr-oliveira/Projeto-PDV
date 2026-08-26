78a6dd7f7e708854afdeebdb3e7cc0457f785e55
Isabelly Oliveira <isabelly.r060408@gmail.com>
2026-08-26 16:33:12 -0300

    upgrade cod

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,9 @@ from app.models import categoria
 from app.models import produto
 from app.models import usuarios
 from app.models import movimentacao
+from app.models import cliente
+from app.models import venda
+
 
 # Gerar a migration
 
